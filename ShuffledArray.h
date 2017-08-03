e39142cc59e74a5523dce3ade7a4c45e69034d95
Steven Tupper <10thElement@gmail.com>
2017-08-03 12:10:15 +0200

    Added special memory scrambling array for char arrays

--- ShuffledArray.h
+++ ShuffledArray.h
@@ -202,4 +202,197 @@ class ShuffledArray{
 			return index;
 		}
 };
+
+template <>
+class ShuffledArray<char>{
+	protected:
+		
+		int count;
+		
+		Random::RandomK random;
+		
+		ShuffledArray(){}
+	public:
+		char * arr;
+		
+		/*
+			arr = data array
+			count = num items in array
+			key = key used to shuffle and unshuffle array
+			random = a instance of the class Random::RandomK (or a derived class)
+			which is used for random generation.
+		*/
+		ShuffledArray(char * arr, int count, int key, Random::RandomK random){
+			std::cout << "Special char scrambled instantiated" << std::endl;
+			this->arr = new char[count *  4];
+			
+			for (int i = 0; i<count; ++i){
+				for (int j = 0; j<4; ++j){
+					this->arr[i*4 + j] = arr[i];
+				}
+			}
+			
+			this->count = count;
+			this->random = random;
+			
+			/* int numBytesPerObject = sizeof(T);
+			
+			for (int i = 0; i<count * numBytesPerObject; ++i){
+				arr[i];
+			} */
+			
+			ShuffleData(key);
+		}
+				
+		char get(int index, int key) {
+			assert(index >= 0 && index<=count);
+			//UnShuffleData(key);
+			
+			/*
+				instead of unshuffling data, just find the index of the
+				item in the shuffled array
+			*/
+			index = GetShiftedIndex(index, key);
+			
+			char result = arr[index]; 
+			//ShuffleData(key);
+			
+			return result;
+		}
+				
+		virtual void Set(int index, int key, char item){
+			assert(index >= 0 && index<=count);
+			//UnShuffleData(key);
+			
+			/*
+				instead of unshuffling data, just find the index of the
+				item in the shuffled array
+			*/
+			index = GetShiftedIndex(index, key);
+			
+			arr[index] = item;
+			//ShuffleData(key);
+		}
+
+		virtual void ShuffleData(int key){
+			std::cout << "Shuffle array" <<std::endl;
+			//get count*2 random ints
+			int * shuffleIndexes = random.GetKRandomInt(key, count*4);
+			
+			/*
+				Clamp the random indexes to the range occupied by the array
+			*/
+			
+			for (int i = 0; i<count*4; ++i){
+				shuffleIndexes[i]%=count*4;
+				if (shuffleIndexes[i]<0){
+					shuffleIndexes[i] += count*4;
+				}
+				//std::cout << shuffleIndexes[i] << " ";
+			}
+			
+			/*
+				Shuffle data
+			*/
+			char temp;
+			
+			for (int i = 0; i<count*4; ++i){
+				//std::cout << shuffleIndexes[i*2] << " shuffleIndexes[i*2]" <<std::endl;
+				//std::cout << shuffleIndexes[i*2+1] << " shuffleIndexes[i*2+1]" <<std::endl;
+				temp = arr[shuffleIndexes[i]];
+				arr[shuffleIndexes[i]] = arr[i];
+				arr[i] = temp;
+				//std::cout << i << " i" <<std::endl;
+			}
+			
+			std::cout << "Shuffled array: ";
+			for (int i = 0; i<count*4; ++i){
+				std::cout << arr[i] << " ";
+			}
+			std::cout << std::endl;
+			
+			delete [] shuffleIndexes;
+		}
+				
+		virtual void UnShuffleData(int key){
+			std::cout << "Unshuffle array" <<std::endl;
+			//get random indexes
+			int * shuffleIndexes = random.GetKRandomInt(key, count*4);
+			
+			/*
+				Clamp the random indexes to the range occupied by the array
+			*/
+			
+			for (int i = 0; i<count*4; ++i){
+				shuffleIndexes[i]%=count*4;
+				if (shuffleIndexes[i]<0){
+					shuffleIndexes[i] += count*4;
+				}
+				//std::cout << shuffleIndexes[i] << " ";
+			}
+			
+			//std::cout << std::endl;
+			
+			/*
+				Shuffle items
+			*/
+			char temp;
+			
+			for (int i = count*4 - 1; i>=0; --i){
+				//std::cout << shuffleIndexes[i*2] << " shuffleIndexes[i*2]" <<std::endl;
+				//std::cout << shuffleIndexes[i*2+1] << " shuffleIndexes[i*2+1]" <<std::endl;
+				temp = arr[shuffleIndexes[i]];
+				arr[shuffleIndexes[i]] = arr[i];
+				arr[i] = temp;
+				//std::cout << i << " i" <<std::endl;
+			}
+			
+			std::cout << "Unshuffled array: ";
+			for (int i = 0; i<count*4; ++i){
+				std::cout << arr[i] << " ";
+			}
+			std::cout << std::endl;
+			
+			delete [] shuffleIndexes;
+		}
+		
+		/*
+			Get item at index by finding where the shuffling process moves it 
+			in array
+		*/
+		virtual int GetShiftedIndex(int index, int key){
+			int * shuffleIndexes = random.GetKRandomInt(key, count*4);
+			
+			for (int i = 0; i<count*4; ++i){
+				shuffleIndexes[i]%=count*4;
+				if (shuffleIndexes[i]<0){
+					shuffleIndexes[i] += count*4;
+				}
+			}
+			
+			/*
+				Goes through the shuffling indexes. When 
+				index shuffleIndexes[i*2] needs to be swapped with 
+				shuffleIndexes[i*2+1] and index === shuffleIndexes[i*2]
+				or shuffleIndexes[i*2+1] == index, index must be 
+				assigned to the shuffle index value. For example if we 
+				start with index 3 and it is swapped with index 7, 
+				we now want to find where index 7 is swapped with another
+				index
+			*/
+			index = index * 4;
+			
+			for (int i = 0; i<count*4; ++i){
+				if (i == index){
+					index = shuffleIndexes[i];
+				} else if (shuffleIndexes[i] == index){
+					index = i;
+				}
+			}
+			
+			delete [] shuffleIndexes;
+			
+			return index;
+		}
+};
 #endif
